4174542874d4fdd6d1327d1bf02c0cbf694d2d21
Raúl Baeza <raul.baeza@3078@alumnos.udg.mx>
2020-09-28 14:48:25 -0500

    prueba01

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-print("Calcular el area del cuadrado\n")
-
-lado = int(input("Ingresa el lado del cuadrado: "))
-area = lado * lado
-print("Area del cuadrado: ",area)
+lado = int(input("Lado: "))
+print (lado * lado)
